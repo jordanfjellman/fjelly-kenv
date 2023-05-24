03ba18ac4f09fc122e841fff1bc1c8b94907c82b
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2023-05-23 22:36:38 -0500

    Initial commit

---
+++ scripts/fjelly-example.js
@@ -0,0 +1,14 @@
+/**
+## Just playin' around at this point.
+*/
+
+// Name: Fjelly Example
+// Description: This is an example script that I used to learn all-the-things.
+// Author: Jordan Fjellman
+// Twitter: @jordanfjellman
+
+// Note: This script could be deleted at any point.
+
+import "@johnlindquist/kit"
+
+browse("https://fjelly.link/home")
